54eaea846f19a0c26c54d56e99809936a5f13775
Andrei <soprachev@mail.ru>
2022-10-13 04:03:58 +0300

    fix page storeage

--- src/components/bottomSheet/usePageStore.ts
+++ src/components/bottomSheet/usePageStore.ts
@@ -3,10 +3,10 @@ import { Ref, ref } from "vue"
 const storage = {}
 
 export function usePageStore<T>(page: string, name: string, value: T): Ref<T> {
-  if (storage[page]?.[name]) {
-    return storage[page][name]
-  } else {
+  if (!storage[page]?.[name]) {
     if (!storage[page]) storage[page] = {}
     storage[page][name] = ref(value)
   }
+
+  return storage[page][name]
 }
